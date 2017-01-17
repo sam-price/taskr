4bebc3d076394eb065c9a8ff5fce95243010a5c6
sam-price <samprice.ethos@gmail.com>
2017-01-17 18:25:25 +0000

    Added flash & error messages and show action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,14 +6,21 @@ class TasksController < ApplicationController
 
 	def create
 		@task = Task.new(tasks_params)
-		@task.save
+		if @task.save
+			flash[:notice] = "Task was successfully created"
+			# Tasks path is found in rake routes. @task is added as route needs the specific ID
+			redirect_to task_path(@task)
+		else
+			# renders original form back to user
+			render 'new'
+		end	
+	end
 
-		# Tasks path is found in rake routes. @task is added as route needs the specific ID
-		redirect_to task_path(@task)
+	def show
+		@task = Task.find(params[:id])
 	end
 
 	private
-
 	# Created new method for security. Saying that we want to require for the tasks
 	# content to be permitted. A blog application would require title/desc etc. to be
 	# required. This is called WHITELISTING.
