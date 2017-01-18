c0e5bb8e9b487f2c5ad4cf383db073e810ba0fcc
sam-price <samprice.ethos@gmail.com>
2017-01-18 21:37:10 +0000

    added destroy action, form partial and fixed redundancy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+	# Uses set_task bit of code only with certain methods
+	before_action :set_task, only:[:edit, :update, :show, :destroy]
 
 	def index
 		@tasks = Task.all
@@ -9,16 +11,14 @@ class TasksController < ApplicationController
 	end
 
 	def edit
-		@task = Task.find(params[:id])
+		
 	end
-
-
 
 	# Handles the new form submission
 	def create
 		@task = Task.new(tasks_params)
 		if @task.save
-			flash[:notice] = "Task was successfully created"
+			flash[:notice] = "Task was successfully created."
 			# Tasks path is found in rake routes. @task is added as route needs the specific ID
 			redirect_to task_path(@task)
 		else
@@ -29,10 +29,10 @@ class TasksController < ApplicationController
 
 	# Handles the edit form submission
 	def update
-		@task = Task.find(params[:id])
+		
 		# Always remmber to whitelist e.g. send to task_params
 		if @task.update(tasks_params)
-			flash[:notice] = "Task was successfully updated"
+			flash[:notice] = "Task was successfully updated."
 			redirect_to task_path(@task)
 		else
 			# Renders original edit form back to user
@@ -41,12 +41,24 @@ class TasksController < ApplicationController
 	end
 
 	def show
-		@task = Task.find(params[:id])
+		
+	end
+
+	def destroy
+		
+		@task.destroy
+		flash[:notice] = "Task was successfully deleted."
+		redirect_to tasks_path
 	end
 
 	
 
 	private
+		
+	# Bit of code repeated throughout different methods	
+	def set_task
+		@task = Task.find(params[:id])
+	end
 	# Created new method for security. Saying that we want to require for the tasks
 	# content to be permitted. A blog application would require title/desc etc. to be
 	# required. This is called WHITELISTING.
